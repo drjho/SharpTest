5aa765f2200df0031897039003fea065260d50b3
m97_j <m97_j@DESKTOP-QUMH9HN>
2016-09-20 23:16:58 +0200

    first commit

---
+++ WhereTest/WhereTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g = new MyGenericClass<TempClass>();
+            var v = ChangeType<string, int>("10");
+        }
+        static T2 ChangeType<T1, T2>(T1 value) where T2 : new()
+        {
+            return (T2)Convert.ChangeType(value, typeof(T2));
+        }
+    }
+
+    public class MyGenericClass<T> where T : IComparable, new() { }
+
+    public class TempClass : IComparable
+    {
+        public int CompareTo(object obj)
+        {
+            return 0;
+        }
+    }
+
+}
